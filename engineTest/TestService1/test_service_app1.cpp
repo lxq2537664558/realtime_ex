b96065eacf3b0049dac944ded9a588dcecfa5e51
bruan <379171482.qq.com>
2016-04-25 15:05:19 +0800

    修复远程调用bug

--- engineTest/TestService1/test_service_app1.cpp
+++ engineTest/TestService1/test_service_app1.cpp
@@ -5,10 +5,31 @@
 #include "test_service_app1.h"
 
 #include "libCoreCommon/message_registry.h"
+#include "libCoreCommon/cluster_invoker.h"
+#include "libCoreCommon/base_app.h"
+#include "libCoreCommon/load_balance_policy.h"
+
+#include "../proto_src/service_request_msg.pb.h"
+#include "../proto_src/client_request_msg.pb.h"
+#include "../proto_src/service_response_msg.pb.h"
+#include "../proto_src/client_response_msg.pb.h"
 
 void client_request_msg_callback(const core::SClientSessionInfo& sClientSessionInfo, uint32_t nMessageType, const google::protobuf::Message* pMessage)
 {
+	const test::client_request_msg* pClientMsg = dynamic_cast<const test::client_request_msg*>(pMessage);
+	DebugAst(pClientMsg != nullptr);
 
+	test::service_request_msg msg;
+	msg.set_name(pClientMsg->name());
+
+	core::CClusterInvoker::Inst()->invok_r(&msg, core::CBaseApp::Inst()->getLoadBalancePolicy(eLBPID_Rand), 0, [sClientSessionInfo](uint32_t nMessageType, const google::protobuf::Message* pMessage, core::EResponseResultType eType)->void
+	{
+		const test::service_response_msg* pResponseMsg = dynamic_cast<const test::service_response_msg*>(pMessage);
+		DebugAst(pResponseMsg != nullptr);
+		test::client_response_msg msg;
+		msg.set_name(pResponseMsg->name());
+		core::CClusterInvoker::Inst()->send(sClientSessionInfo, &msg);
+	});
 }
 
 CTestServiceApp1::CTestServiceApp1()
@@ -27,7 +48,7 @@ CTestServiceApp1* CTestServiceApp1::Inst()
 bool CTestServiceApp1::onInit()
 {
 	core::CMessageRegistry::Inst()->registerGateForwardCallback("test.client_request_msg", &client_request_msg_callback);
-
+	core::CMessageRegistry::Inst()->registerServiceCallback("test.service_response_msg", nullptr);
 	return true;
 }
 
--- engineTest/TestService2/test_service_app2.cpp
+++ engineTest/TestService2/test_service_app2.cpp
@@ -5,10 +5,24 @@
 #include "test_service_app2.h"
 
 #include "libCoreCommon/message_registry.h"
+#include "libCoreCommon/cluster_invoker.h"
+#include "libCoreCommon/base_app.h"
+#include "libCoreCommon/load_balance_policy.h"
+
+#include "../proto_src/service_request_msg.pb.h"
+#include "../proto_src/client_request_msg.pb.h"
+#include "../proto_src/service_response_msg.pb.h"
+#include "../proto_src/client_response_msg.pb.h"
 
 void service_request_msg_callback(const std::string& szFromService, uint32_t nMessageType, const google::protobuf::Message* pMessage)
 {
+	const test::service_request_msg* pRequestMsg = dynamic_cast<const test::service_request_msg*>(pMessage);
+	DebugAst(pRequestMsg != nullptr);
 
+	test::service_response_msg msg;
+	msg.set_name(pRequestMsg->name());
+
+	core::CClusterInvoker::Inst()->response(&msg);
 }
 
 CTestServiceApp2::CTestServiceApp2()
